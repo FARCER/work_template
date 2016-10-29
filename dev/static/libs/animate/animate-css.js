8c1986e99b00debd7a26e4e785421c92c652f9c8
FARCER <counter18@yandex.ru>
2016-10-29 14:16:54 +0300

    git init

---
+++ dev/static/libs/animate/animate-css.js
@@ -0,0 +1,21 @@
+;(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						};
+				}, {
+						offset: "80%"
+				}).waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						};
+				}, {
+						offset: -$(window).height()
+				});
+		};
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,161 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    browsersync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    stylus = require('gulp-stylus'),
+    cache = require('gulp-cache'),
+    spritesmith = require("gulp.spritesmith"),
+    plumber = require("gulp-plumber"),
+    notify = require("gulp-notify"),
+    newer = require("gulp-newer"),
+    autoprefixer = require('gulp-autoprefixer');
+
+// Работа со Stylus
+gulp.task('stylus', function() {
+    return gulp.src([
+            'dev/static/stylus/main.styl',
+        ])
+        .pipe(plumber())
+        .pipe(stylus({
+            compress: true
+        }))
+
+    .on("error", notify.onError({
+            message: "Ошибка в Stylus",
+            title: "Ошибка в Stylus"
+        }))
+        .pipe(autoprefixer(['last 2 version']))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dev/static/css'))
+        .pipe(browsersync.reload({
+            stream: true
+        }));
+});
+
+// Работа с Pug
+gulp.task('pug', function() {
+    return gulp.src('dev/pug/pages/*.pug')
+        .pipe(plumber())
+        .pipe(pug())
+        .on("error", notify.onError({
+            message: "Ошибка в Pug",
+            title: "Ошибка в Pug"
+        }))
+        .pipe(gulp.dest('dev'));
+});
+
+// Browsersync
+gulp.task('browsersync', function() {
+    browsersync({
+        server: {
+            baseDir: 'dev'
+        },
+    })
+});
+
+// Работа с JS
+gulp.task('scripts', function() {
+    return gulp.src([
+            // Библиотеки
+            'dev/static/libs/jquery/jquery.js',
+            'dev/static/libs/magnific/jquery.magnific-popup.min.js',
+            'dev/static/libs/bxslider/jquery.bxslider.min.js',
+            // 'dev/static/libs/animate/animate-css.js',
+            'dev/static/libs/validate/jquery.validate.min.js',
+            // Свои файлы
+            'dev/static/js/main.js'
+        ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dev/static/js'))
+        .pipe(browsersync.reload({
+            stream: true
+        }));
+});
+
+
+// Сборка спрайтов PNG
+gulp.task('cleansprite', function() {
+    return del.sync('dev/static/img/sprite/sprite.png');
+});
+
+
+gulp.task('spritemade', function() {
+    var spriteData =
+        gulp.src('dev/static/img/sprite/*.*')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_sprite.styl',
+            padding: 15,
+            cssFormat: 'stylus',
+            algorithm: 'binary-tree',
+            cssTemplate: 'stylus.template.mustache',
+            cssVarMap: function(sprite) {
+                sprite.name = 's-' + sprite.name
+            }
+        }));
+
+    spriteData.img.pipe(gulp.dest('dev/static/img/sprite/')); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest('dev/static/stylus/')); // путь, куда сохраняем стили
+});
+gulp.task('sprite', ['cleansprite', 'spritemade']);
+// Слежение
+gulp.task('watch', ['browsersync', 'stylus', 'scripts'], function() {
+    gulp.watch('dev/static/stylus/**/*.styl', ['stylus']);
+    gulp.watch('dev/pug/**/*.pug', ['pug']);
+    gulp.watch('dev/*.html', browsersync.reload);
+    gulp.watch(['dev/static/js/*.js', '!dev/static/js/scripts.min.js*'], ['scripts']);
+});
+
+// Очистка папки сборки
+gulp.task('clean', function() {
+    return del.sync('prodact');
+});
+
+// Оптимизация изображений
+gulp.task('img', function() {
+    return gulp.src(['dev/static/img/**/*', '!dev/static/img/sprite/*'])
+        .pipe(cache(imagemin({
+            progressive: true,
+            use: [pngquant()]
+
+        })))
+        .pipe(gulp.dest('product/static/img'))
+});
+
+// Сборка проекта
+
+gulp.task('build', ['clean', 'img', 'stylus', 'scripts'], function() {
+    var buildCss = gulp.src('dev/static/css/*.css')
+        .pipe(gulp.dest('product/static/css'))
+
+    var buildFonts = gulp.src('dev/static/fonts/**/*')
+        .pipe(gulp.dest('product/static/fonts'))
+
+    var buildJs = gulp.src('dev/static/js/scripts.min.js')
+        .pipe(gulp.dest('product/static/js'))
+
+    var buildHtml = gulp.src('dev/*.html')
+        .pipe(gulp.dest('product/'))
+
+    var buildImg = gulp.src('dev/static/img/sprite/sprite.png')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('product/static/img/sprite/'))
+});
+
+// Очистка кеша
+gulp.task('clear', function() {
+    return cache.clearAll();
+});
+
+// Дефолтный таск
+gulp.task('default', ['watch']);
